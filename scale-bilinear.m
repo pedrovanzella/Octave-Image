eb3901b9d13da5c5ad90a8e560a5e034cf58dfc3
Pedro Vanzella <pedro@pedrovanzella.com>
2015-06-15 10:26:49 -0300

    meshing

--- scale-bilinear.m
+++ scale-bilinear.m
@@ -1,2 +1,9 @@
 pkg load image;
+scale = 1.25;
+
 I = imread("cow_small.png");
+inRows = rows(I);
+inCols = columns(I);
+
+zz = 1./(inRows.^2 + inCols^2 + 1);
+[xxi, yyi] = meshgrid(1:(1/scale):size(zz,1), 1:(1/scale):size(zz,2));
