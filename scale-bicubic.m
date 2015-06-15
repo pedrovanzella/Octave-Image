994474957a0cd72a50cc998a3d1022a7491b3f50
Pedro Vanzella <pedro@pedrovanzella.com>
2015-06-15 10:59:49 -0300

    cubic not working

---
+++ scale-bicubic.m
@@ -0,0 +1,17 @@
+pkg load image;
+scale = 2;
+
+I = imread("cow_very_small.png");
+inRows = rows(I);
+inCols = columns(I);
+
+outRows = ceil(inRows * scale);
+outCols = ceil(inCols * scale);
+
+[XI, YI] = meshgrid (linspace (1, inCols, outCols), linspace (1, inRows, outRows));
+
+for i = 3:-1:1
+          nI(:, :, i) = interp2(I(:, :, i), XI, YI, "cubic", NA);
+endfor
+
+imwrite(nI, "cow_cubic.png");
